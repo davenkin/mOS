80c5c93089651154b5e1657edd30f685846a8a22
Davenkin <davenkin@163.com>
2012-12-28 18:07:21 +0800

    try to return a list of map for resultset

--- opinions-persistence/src/main/java/davenkin/opinions/persistence/JdbcSurveyDao.java
+++ opinions-persistence/src/main/java/davenkin/opinions/persistence/JdbcSurveyDao.java
@@ -9,6 +9,7 @@ import org.apache.log4j.Logger;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,100 +18,141 @@ import java.util.List;
  * Time: 10:45 PM
  * To change this template use File | Settings | File Templates.
  */
-public class JdbcSurveyDao implements SurveyDao {
+public class JdbcSurveyDao implements SurveyDao
+{
     private DataSource dataSource;
     private final Logger logger = Logger.getLogger(this.getClass());
     ;
 
-    public JdbcSurveyDao(DataSource dataSource) {
+    public JdbcSurveyDao(DataSource dataSource)
+    {
         this.dataSource = dataSource;
     }
 
-    public Survey findSurveyById(Long surveyId) {
+    public Survey findSurveyById(Long surveyId)
+    {
 
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public List<Survey> findAllSurveys() {
+    public List<Survey> findAllSurveys()
+    {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public List<Survey> findSurveysCreatedByUser(Long userId) {
+    public List<Survey> findSurveysCreatedByUser(Long userId)
+    {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public List<Survey> findSurveysByCategory(String category) {
+    public List<Survey> findSurveysByCategory(String category)
+    {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public List<Survey> findSurveysByTag(String tag) {
+    public List<Survey> findSurveysByTag(String tag)
+    {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public List<Survey> findSurveysCreatedBetween(Date fromDate, Date toDate) {
+    public List<Survey> findSurveysCreatedBetween(Date fromDate, Date toDate)
+    {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public List<String> findTagsForSurvey(Long surveyId) {
+    public List<String> findTagsForSurvey(Long surveyId)
+    {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public List<SurveyOption> findOptionsForSurvey(Long surveyId) {
+    public List<SurveyOption> findOptionsForSurvey(Long surveyId)
+    {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public List<SurveyComment> findCommentsForSurvey(Long surveyId) {
+    public List<SurveyComment> findCommentsForSurvey(Long surveyId)
+    {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public User findUserById(Long userId) {
+    public User findUserById(Long userId)
+    {
+//        return queryForListOfMapS(userId, null);
+        return null;
+    }
+
+    private List<Map> queryForListOfMapS(Long userId, PreparedStatement preparedStatement)
+    {
         Connection connection = null;
-        try {
+        ResultSet resultSet = null;
+        try
+        {
             connection = dataSource.getConnection();
             logger.info("Use connection: " + connection.hashCode());
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM USER WHERE ID = ?");
+            preparedStatement = connection.prepareStatement("SELECT * FROM USER WHERE ID = ?");
             preparedStatement.setLong(1, userId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            while (resultSet.next()) {
-                long id = resultSet.getLong("ID");
-                String name = resultSet.getString("NAME");
-                String email = resultSet.getString("EMAIL");
-                String password = resultSet.getString("PASSWORD");
-                Timestamp register_time = resultSet.getTimestamp("REGISTER_TIME");
-                User user = new User(id);
-                user.setName(name);
-                user.setEmail(email);
-                user.setPassword(password);
-                user.setRegisterTime(register_time);
-                logger.info("Load User: ID = " + id + ", Name = " + name);
-                return user;
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next())
+            {
+//                return createUser(resultSet);
             }
 
-        } catch (SQLException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } finally {
-
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                }
-            }
+            closeResources(connection, resultSet, preparedStatement);
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
         }
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+
+        return null;
     }
 
-    public void takeSurvey(Long surveyId, Long optionId) {
+    private User createUser(ResultSet resultSet) throws SQLException
+    {
+        long id = resultSet.getLong("ID");
+        String name = resultSet.getString("NAME");
+        String email = resultSet.getString("EMAIL");
+        String password = resultSet.getString("PASSWORD");
+        Timestamp register_time = resultSet.getTimestamp("REGISTER_TIME");
+        User user = new User(id);
+        user.setName(name);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRegisterTime(register_time);
+        logger.info("Load User: ID = " + id + ", Name = " + name);
+        return user;
+    }
+
+    private void closeResources(Connection connection, ResultSet resultSet, PreparedStatement preparedStatement) throws SQLException
+    {
+        if (resultSet != null)
+        {
+            resultSet.close();
+        }
+        if (preparedStatement != null)
+        {
+            preparedStatement.close();
+        }
+        if (connection != null)
+        {
+            connection.close();
+        }
+    }
+
+    public void takeSurvey(Long surveyId, Long optionId)
+    {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public void removeSurvey(Long surveyId) {
+    public void removeSurvey(Long surveyId)
+    {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public void removeComment(Long commentId) {
+    public void removeComment(Long commentId)
+    {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 }
