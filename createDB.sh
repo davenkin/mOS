f73ae76ba67d018134da73d883f44ddbb7e92808
Davenkin <davenkin@gmail.com>
2013-04-30 17:10:55 +0800

    use embedded database for testing

--- createDB.sh
+++ createDB.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 echo "Start to create database..."
-mysql -u root OPINIONS < opinions-persistence/config/create_tables_mysql.sql
-mysql -u root OPINIONS < opinions-persistence/config/create_data_mysql.sql
+mysql -u root OPINIONS < opinions-core/config/create_tables_mysql.sql
+mysql -u root OPINIONS < opinions-core/config/create_data_mysql.sql
 echo "Finished creating databaase..."
